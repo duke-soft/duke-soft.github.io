8177e32abe5373aca09c809e288eedcb411e246f
Luke Dykstra <lukedykstr@gmail.com>
2025-04-08 01:10:33 -0400

    add AOK

--- AOK/script.js
+++ AOK/script.js
@@ -1,75 +1,177 @@
-function load() {
-  var fileInput = document.getElementById("file");
+/**
+ * @author: Luke Dykstra
+ * @description: This script creates a simple social media post layout using JavaScript.
+ */
 
-  if (fileInput.files.length > 0) {
-    var reader = new FileReader();
-    output.innerHTML = "";
-    var incShorts = document.getElementById("yes").checked;
+// Define width of the post images in pixels
+const post_width_value = 440
+const post_width = post_width_value + 'px'
+const post_img_count = 9
+const init_post_count = 20
+const genres = ['nature', 'humor', 'motivational', 'educational']
 
-    reader.addEventListener("load", (e) => {
-      print("parsing input data...");
+// Array to store post objects
+// Each post object contains the source of the image, profile name, and genre
+let posts = []
+let used_imgs = []
 
-      var history = JSON.parse(reader.result);
+const posts_container     = document.getElementById('posts-container')
+const likes_stats_label   = document.getElementById('likes')
+const unlikes_stats_label = document.getElementById('unlikes')
 
-      print("done");
-      print("counting per year...");
+// Function to create a new post image element given a source URL
+function newPostImage(src) {
+    let img = document.createElement('img')
+    img.src = src
+    img.style.width = post_width
+    img.style.height = 'auto'
+    img.style.borderRadius = '12px'
 
-      var yearCounts = {};
-      var currYear = new Date().getFullYear();
+    return img
+}
 
-      for (var propYear = 2005; propYear <= currYear; propYear++) {
-        yearCounts[propYear] = 0;
-      }
+// Function to create a new image element given a source URL
+function newImage(src) {
+    let img = document.createElement('img')
+    img.src = src
 
-      var total = 0;
+    return img
+}
 
-      for (var i = 0; i < history.length; i++) {
-        if (history[i].titleUrl != undefined && history[i].titleUrl.substring(8, 13) == "music") { continue; }
+// Function to create a new like button element given a post ID
+function newLikeButton(post_id) {
+    const like_btn = document.createElement('img')
+    like_btn.src = 'images/heart-btn-outline.svg'
+    like_btn.style.width  = '30px'
+    like_btn.style.height = 'auto'
 
-        yearCounts[history[i].time.substring(0, 4)]++;
-        total++;
-      }
+    like_btn.addEventListener('click', () => {
+        const heart_outline = 'images/heart-btn-outline.svg';
+        const heart_filled  = 'images/heart-btn-filled.svg';
+        const current_src   = like_btn.src.split('/').pop();
 
-      print("done");
-      print("=========== RESULTS ===========");
-
-      var start = false;
-
-      for (let year in yearCounts) {
-        if (yearCounts[year] != 0) { start = true; }
-
-        if (start) {
-          print(year + ": " + yearCounts[year] + ((incShorts && (year == "2020")) ? "   [YT Shorts created]" : ""));
-
-          if (incShorts && (parseInt(year) >= 2020)) {
-            print("  - est. #non-shorts: " + Math.round(yearCounts[year] * 0.071));
-          }
+        if (current_src == 'heart-btn-outline.svg') {
+            like_btn.src = heart_filled
+            posts[post_id].likes += 1
+            analytics.total_likes += 1
+            analytics.post_stats[post_id].user_likes += 1
+        } else {
+            like_btn.src = heart_outline
+            posts[post_id].likes -= 1
+            analytics.total_unlikes += 1
+            analytics.post_stats[post_id].user_unlikes += 1
+            analytics.post_stats[post_id].user_likes -= 1
         }
-      }
 
-      var totalTime = total * 11.7;
-      var nsTotal = Math.round(total * 0.071);
-      var nsTotalTime = Math.round(nsTotal * 11.7);
+        document.getElementById('likes-' + post_id).innerHTML = posts[post_id].likes
+    })
 
-      print("===============================");
-      print("TOTAL: " + total);
-
-      if (incShorts) {
-        print("est. non-short TOTAL: " + nsTotal);
-        print("est. time watched: " + nsTotalTime + " min");
-        print(" ~" + (Math.round(nsTotalTime / 60)) + " hrs");
-      } else {
-        print("est. time watched: " + totalTime + " min");
-        print(" ~" + (Math.round(totalTime / 60)) + " hrs");
-      }
-    });
-
-    reader.readAsText(fileInput.files[0]);
-  }
+    return like_btn
 }
 
-function print(text) {
-  var output = document.getElementById("output");
-  output.innerHTML += text + "<br>";
-  console.log(text);
+// Function to create a new post element given a post ID
+// The post ID is the index of the post in the posts array
+function newPostElement(post_id) {
+    const post_data = posts[post_id]
+    const post_div = document.createElement('div')
+
+    const post_img = newPostImage(post_data.src)
+    post_img.id = post_id
+
+    post_div.appendChild(post_img)
+    post_div.appendChild(document.createElement('br'))
+
+    const prof_padding = 7
+    const prof_div = document.createElement('div')
+    prof_div.style.display      = 'flex'
+    prof_div.style.alignItems   = 'center'
+    prof_div.style.padding      = prof_padding + 'px'
+    prof_div.style.maxWidth     = (post_width_value - (prof_padding * 2)) + 'px'
+    prof_div.style.borderRadius = '10px'
+    prof_div.style.backgroundColor = '#c8d4e8'
+
+    const prof_pic = newPostImage('images/profile-icon.png')
+    prof_pic.style.width  = '40px'
+    prof_pic.style.height = 'auto'
+    prof_div.appendChild(prof_pic)
+
+    const prof_name = document.createElement('p')
+    prof_name.innerHTML = post_data.profile
+    prof_name.style.marginLeft = '10px'
+    prof_div.appendChild(prof_name)
+
+    const likes = document.createElement('p')
+    likes.id = 'likes-' + post_id
+    likes.innerHTML = post_data.likes
+    likes.style.marginLeft = 'auto'
+    likes.style.marginRight = '10px'
+    prof_div.appendChild(likes)
+    prof_div.appendChild(newLikeButton(post_id))
+
+    post_div.appendChild(prof_div)
+    return post_div
 }
+
+// Function to create a new post object and add it to the posts array given an image source, profile name, and genre
+function createPost(src, profile, genre) {
+    const post = {
+        src: src,
+        profile: profile,
+        genre: genre,
+        likes: Math.floor(Math.random() * 1001)
+    }
+
+    posts.push(post)
+    return posts.length - 1
+}
+
+// Function to generate a specified number of posts with random images and profiles
+function generatePosts(count) {
+    for (let i = 0; i < count; i ++) {
+        let src   = ''
+        let name  = ''
+        let genre = ''
+
+        do {
+            const profile = profiles[Math.floor(Math.random() * profiles.length)]
+            name  = profile.name
+            genre = profile.genre
+
+            if (profile.genre == 'misc') {
+                genre = genres[Math.floor(Math.random() * genres.length)]
+            }
+
+            const src_index = Math.floor(Math.random() * post_img_count) + 1
+            src = 'images/posts/' + genre + '/' + genre + src_index + '.jpg'
+        
+        } while (used_imgs.includes(src))
+        used_imgs.push(src)
+
+        createPost(src, name, genre)
+    }
+}
+
+// Function to generate post elements starting from a specified index in the posts array
+function generatePostElements(start_index) {
+    for (let i = start_index; i < posts.length; i ++) {
+        const post = newPostElement(i)
+        posts_container.appendChild(post)
+        posts_container.appendChild(document.createElement('br'))
+
+        analytics.post_stats[i] = {
+            check_time: -1,
+            view_time: 0,
+            last_view_time: 0,
+            user_likes: 0,
+            user_unlikes: 0
+        }
+    }
+}
+
+// Function to clear the used images array
+function clearUsedImgs() {
+    used_imgs = []
+}
+
+generatePosts(init_post_count)
+generatePostElements(0)
